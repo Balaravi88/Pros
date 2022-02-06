8c83d1db05a78c89e27dec4012a731bc78eaf5ff
Balaravi88 <balaravi8895@gmail.com>
2022-02-06 19:35:14 +0530

    Balaji Ready

--- src/org/login/Pro.java
+++ src/org/login/Pro.java
@@ -1,13 +1,28 @@
 package org.login;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Pro {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int i=0;
-do {
-	System.out.println(i);
-	i++;
-} while (i<10);
+public static void main(String[] args) {
+		
+		Set<Integer> s=new LinkedHashSet<>();
+		s.add(10);
+		s.add(20);
+		s.add(30);
+		s.add(40);
+		s.add(50);
+		s.add(10);
+		System.out.println(s);
+	int size=s.size();
+	System.out.println(size);
+		
+s.remove(30);
+System.out.println(s);
+
+	
+	s.contains(30);
+	
 	}
 
 }
